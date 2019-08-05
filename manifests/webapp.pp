6cc4b7a9e520ab03112631e610684a39117be41f
Alexandre Larouche <alexandre.larouche@calculquebec.ca>
2019-08-05 10:22:48 -0400

    Updated webapp.pp so it actually works now

--- manifests/webapp.pp
+++ manifests/webapp.pp
@@ -1,6 +1,7 @@
-class webapp {
-#modify nginx config
-
+class jobs_exporter::webapp {
+  package { 'python36-pip':
+    ensure => 'latest'
+  }
   package { 'python36-requests':
     ensure => 'latest'
   }
@@ -10,53 +11,44 @@ class webapp {
   package { 'texlive-lastpage':
     ensure => 'latest'
   }
-
   package { 'flask' :
     ensure => 'latest',
     provider => 'pip3'
   }
-
   package { 'pylatex':
     ensure => 'latest',
     provider => 'pip3',
     require => Package['texlive']
   }
-
   package { 'requests':
     ensure => 'latest',
     provider => 'pip3'
   }
-
   package { 'matplotlib':
     ensure => 'latest',
     provider => 'pip3'
   }
-
   file { 'logic_webapp':
     ensure => 'present',
-    path => '/centos/logic_webapp'
+    path => '/centos/logic_webapp',
     source => "puppet:///modules/jobs_exporter/logic_webapp.py"
   }
-
   file { 'job.py':
     ensure => 'present',
-    path => '/centos/job.py'
+    path => '/centos/job.py',
     source => "puppet:///modules/jobs_exporter/job.py"
   }
-
   file { '/centos/pdf':
     ensure => 'directory',
   }
-
   file { '/centos/plots':
     ensure => 'directory',
   }
-
   file { '/centos/pies':
-    ensure => 'directory'
+    ensure => 'directory',
   }
-
-  exec { '/usr/bin/python36 /centos/logic_webapp'
+  exec { 'start logic_webapp':
+    command => '/usr/bin/python36 /centos/logic_webapp',
   }
-
 }
+
